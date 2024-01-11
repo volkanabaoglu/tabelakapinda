5a35a04ce07948c27c4704b2ba337775d1b7f17c
Volkan Abaoglu <abaoglu.2002@gmail.com>
2024-01-11 03:07:49 +0300

    Added new scripts for run client and server - both them

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,26 @@
-import express from 'express';
-import products from './data/products.js';
+import express from "express";
+import products from "./data/products.js";
 
 const port = 5000;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
+app.get("/api/products", (req, res) => {
+  res.json(products);
 });
 
 app.get("/api/products/:id", (req, res) => {
-    const productId = parseInt(req.params.id);
-    const product = products.find(p => p._id === productId);
+  const product = products.find((p) => p._id === req.params.id);
 
-    console.log(product);
-
-    if (!product) {
-        console.log("Not found");
-        res.status(404).json({ message: 'Product not found' });
-    } else {
-        res.json(product);
-    }
+  if (!product) {
+    res.status(404).json({ message: "Product not found" });
+  } else {
+    res.json(product);
+  }
 });
-
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
