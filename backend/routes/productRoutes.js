65b6d98b1720fd9427168ec53a6df0db2a541ff3
Volkan Abaoglu <abaoglu.2002@gmail.com>
2024-01-13 18:07:22 +0300

    Added errorMiddleware file and configured error handling funcs

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,28 +2,27 @@ import express from "express";
 import connectDB from "../config/db.js";
 import env from "dotenv";
 import Product from "../models/productModel.js";
+import asyncHandler from "../middleware/asyncHandler.js";
 
 env.config();
 connectDB();
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  try {
-    const response = await Product.find();
-    res.json(response);
-  } catch (error) {
-    res.status(404).json({ message: "Products not found!" });
-  }
-});
+router.get('/',asyncHandler(async(req,res)=>{
+    const products = await Product.find();
+    res.json(products);
+}))
 
-router.get("/:id", async (req, res) => {
-  const productId = req.params.id;
-  try {
-    const response = await Product.findById(productId);
-    res.json(response);
-  } catch (error) {
-    res.status(404).json({ message: "Product not found!" });
-  }
-});
+router.get('/:id',asyncHandler(async(req,res)=>{
+    const product = await Product.findById(req.params.id);
+
+    if(product){
+        return res.json(product);
+    }else{
+        res.status(404);
+        throw new Error('Resource not found')
+    }
+}))
+
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config();
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import {errorHandler , notFound} from "./middleware/errorMiddleware.js"
 
 connectDB();//Connect to DB
 
@@ -14,6 +15,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products",productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
