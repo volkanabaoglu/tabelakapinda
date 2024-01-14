614db8abca2495d808dde720a2ee2c740aafd7ba
Volkan Abaoglu <abaoglu.2002@gmail.com>
2024-01-15 00:40:34 +0300

    Added useGetProductDetailsQuery to product detail screen

--- frontend/src/slices/apiProductSlice.js
+++ frontend/src/slices/apiProductSlice.js
@@ -1,14 +1,21 @@
 import { apiSlice } from "./apiSlice";
+import { PRODUCTS_URL } from "../constants";
 
 export const apiProductSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getProducts: builder.query({
       query: () => ({
-        url: "/api/products",
+        url: PRODUCTS_URL,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+    getProductDetails: builder.query({
+      query: (id) => ({
+        url: `${PRODUCTS_URL}/${id}`,
       }),
       keepUnusedDataFor: 5,
     }),
   }),
 });
 
-export const { useGetProductsQuery } = apiProductSlice;
+export const { useGetProductsQuery  , useGetProductDetailsQuery} = apiProductSlice;
