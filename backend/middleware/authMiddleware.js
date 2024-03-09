4d9ebc0069ac438d81652754cbb136eced7c75fe
Volkan Abaoglu <volkan.abaoglu>
2024-03-09 21:13:55 +0300

    added admin and user middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,26 +1,38 @@
-import { Jwt } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import asyncHandler from "./asyncHandler.js";
 import User from "../models/userModel.js";
 
 // Protect routes
 
-export const protect = asyncHandler(async (req, res, next) => { 
-    let token ;
-    // Read jwt from the cookie
-    token = req.cookies.jwt;
-    if(!token){
-        res.status(401);
-        throw new Error('Not authorized, no token found');
-    }
-    // Verify token
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    
-    try {
-        req.user = await User.findById(decoded.userId).select('-password');
-        next();
-    } catch (error) {
-        res.status(401);
-        throw new Error('Not authorized, token failed');
-    }
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+  // Read jwt from the cookie
+  token = req.cookies.jwt;
+  if (!token) {
+    console.log("No token found");
+    res.status(401);
+    throw new Error("Not authorized, no token found");
+  }
+  // Verify token
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-})
+  try {
+    req.user = await User.findById(decoded.userId).select("-password");
+    next();
+  } catch (error) {
+    res.status(401);
+    throw new Error("Not authorized, token failed");
+  }
+});
+
+// Admin middleware
+
+const admin = asyncHandler(async (req, res, next) => {
+  if (!req.user || !req.user.isAdmin) {
+    res.status(401);
+    throw new Error("Not authorized as an admin");
+  }
+  next();
+});
+
+export { protect, admin };
