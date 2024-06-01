5a14bb7e88e5be25877a7e780b6af50e461f85f8
Volkan Abaoglu <volkan.abaoglu>
2024-06-01 21:28:36 +0300

    chore: Update import statements and dependencies in LoginScreen.jsx

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,73 +1,82 @@
-import React from 'react'
-import { useState , useEffect } from 'react'
-import { Form, Button, Row, Col, Toast } from 'react-bootstrap'
-import { Link , useLocation , useNavigate } from 'react-router-dom'
-import { useDispatch , useSelector } from 'react-redux'
-import { FormContainer } from '../components/FormContainer'
+import React, { useState, useEffect } from 'react';
+import { Form, Button, Row, Col, Toast } from 'react-bootstrap';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import FormContainer from '../components/FormContainer';
 import Loader from '../components/Loader';
-import { useLoginMutation } from '../slices/usersApiSlice'
+import { useLoginMutation } from '../slices/usersApiSlice';
 import { setCredentials } from '../slices/authSlice';
+
 const LoginScreen = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const [login, { isLoading }] = useLoginMutation()
-    const userInfo = useSelector((state) => state.auth.userInfo)
+  const [login, { isLoading }] = useLoginMutation();
+  const userInfo = useSelector((state) => state.auth.userInfo);
 
-    const { search } = useLocation()
-    const sp = new URLSearchParams(search)
-    const redirect = sp.get('redirect')
+  const { search } = useLocation();
+  const sp = new URLSearchParams(search);
+  const redirect = sp.get('redirect');
 
-
-    useEffect(() => {
-        if (userInfo) {
-            navigate(redirect ? redirect : '/')
-        }
-    },[navigate, userInfo , navigate])
-
-    const submitHandler = async (e) => {
-        e.preventDefault()
-        
-        try {
-            const res = await login({email , password}).unwrap();
-            dispatch(setCredentials({...res,}))
-            navigate(redirect)
-        } catch (error) {
-            Toast.error(error?.data?.message || error.error);
-        }
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect ? redirect : '/');
     }
+  }, [navigate, userInfo, redirect]); // Burada 'redirect' eklendi
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await login({ email, password }).unwrap();
+      dispatch(setCredentials({ ...res }));
+      navigate(redirect ? redirect : '/');
+    } catch (error) {
+      Toast.error(error?.data?.message || error.error);
+    }
+  };
+
   return (
     <FormContainer>
-        <h1>Sign In</h1>
+      <h1>Sign In</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId='email' className='mt-4'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
 
-        <Form onSubmit={submitHandler}>
-            <Form.Group controlId='email' className='mt-4'>
-                <Form.Label>Email Address</Form.Label>
-                <Form.Control type='email' placeholder='Enter email' value={email} onChange={(e) => setEmail(e.target.value)}></Form.Control>
-            </Form.Group>
+        <Form.Group controlId='password' className='mt-4'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Enter password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
 
-            <Form.Group controlId='password' className='mt-4'>
-                <Form.Label>Password</Form.Label>
-                <Form.Control type='password' placeholder='Enter password' value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
-            </Form.Group>
+        <Button type='submit' variant='primary' className='mt-4' disabled={isLoading}>
+          Sign In
+        </Button>
 
-            <Button type='submit' variant='primary' className='mt-4'
-            disabled={isLoading}
-            >Sign In</Button>
+        {isLoading && <Loader />}
 
-            {isLoading && <Loader />}
-
-            <Row>
-                <Col className='mt-4'>
-                    New Customer? <Link to='/register'>Register</Link>
-                </Col>
-            </Row>
-        </Form>
+        <Row>
+          <Col className='mt-4'>
+            New Customer? <Link to='/register'>Register</Link>
+          </Col>
+        </Row>
+      </Form>
     </FormContainer>
-  )
-}
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
